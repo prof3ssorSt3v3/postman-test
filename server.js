45736a2b1cad1e8a72569edde4e72ba513bd3aba
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2020-04-26 14:06:32 -0400

    added the api key test

--- server.js
+++ server.js
@@ -9,8 +9,20 @@ app.use(express.json());
 //handle all the routes starting with /api/
 app.use('/api', apiRoutes);
 
-app.get('/', (req, res) => {
+const validateKey = (req, res, next) => {
+  //we could add this middleware to any route we want
+  let keyHeader = req.header('x-api-key');
+  if (keyHeader && keyHeader == 'MyUniqueApiKey') {
+    //we could do whatever we want to validate the key
+    next();
+  } else {
+    res.status(401).send({ error: { code: 999, message: 'Invalid API key' } });
+  }
+};
+
+app.get('/', validateKey, (req, res) => {
   //health check route
+  //check for API key
   res.status(200).send({ STATUS: 'Good to go!' });
 });
 
